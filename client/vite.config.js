467d725bbe11b0c3f8e93eaad1d10639df916e8b
CHANDUDEV69 <iamchandudeva@gmail.com>
2025-07-06 21:58:22 +0530

    slight change

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,14 +4,4 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   base: '/',
-  server: {
-  proxy: {
-    '/api': {
-      target: 'https://mancherial-production.up.railway.app',
-      changeOrigin: true,
-      rewrite: (path) => path.replace(/^\/api/, '')
-    }
-  }
-}
-
 })
