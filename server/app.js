fdc0f40872681d5b48cc0160e15140c055d883ef
CHANDUDEV69 <iamchandudeva@gmail.com>
2025-07-06 14:44:13 +0530

    added server code app.js

---
+++ server/app.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+const path = require('path');
+const { default: connectDB } = require('./db');
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+// Middleware
+app.use(express.json());
+connectDB();
+
+// Serve frontend if built in same project
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, '../client/build')));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, '../client/build', 'index.html'));
+  });
+}
+
+// Example API
+app.get('/api', (req, res) => {
+  res.json({ message: 'API working!' });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
