34b331c54863f3ac693fe90703e4abb7593be36a
CHANDUDEV69 <iamchandudeva@gmail.com>
2025-07-07 19:22:36 +0530

    few changes

--- server/app.js
+++ server/app.js
@@ -15,7 +15,7 @@ await connectDB();
 // Middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.set({"view engine": "ejs"});
+app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 app.use(cors({
   origin: process.env.CLIENT_URL?.split(',') || [
