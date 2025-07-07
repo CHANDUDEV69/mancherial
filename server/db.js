e18452e12827b15ef280421ecb48b7b58e2543ba
CHANDUDEV69 <iamchandudeva@gmail.com>
2025-07-07 10:49:34 +0530

    db code change

--- server/db.js
+++ server/db.js
@@ -1,16 +1,13 @@
 import mongoose from 'mongoose';
-import {MongClient} from "mongoose";
+import {MongClient} from "mongodb";
 const connectDB = async () => {
   let _db;
   try {
-    // await mongoose.connect(process.env.mongoDBURL, {
-    //   useNewUrlParser: true,
-    //   useUnifiedTopology: true,
-    // });
     const client = new MongClient(process.env.mongoDBURL);
     await client.connect();
     _db = client.db("macherial");
     console.log("✅ MongoDB connected");
+    return _db;
   } catch (err) {
     console.error("❌ MongoDB error:", err.message);
     process.exit(1);
