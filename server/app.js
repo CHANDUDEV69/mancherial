eebecdf79789fe8351b21950cdc829c23a31ee4a
CHANDUDEV69 <iamchandudeva@gmail.com>
2025-07-08 16:16:36 +0530

    response

--- server/app.js
+++ server/app.js
@@ -51,7 +51,7 @@ app.post('/contact', async (req, res) => {
       message,
       submittedAt: new Date(),
     }).then((result)=>{
-       res.status(200).send("Successful");
+       return res.status(200);
     }).catch((err)=>{
       console.log(err)
     });
